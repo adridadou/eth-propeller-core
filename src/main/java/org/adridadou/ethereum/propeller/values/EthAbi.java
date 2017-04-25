32fbb7e9a5ca19256ec84cd137a165fd289064ea
adridadou <david@roon.me>
2017-04-25 12:52:42 +0200

    make it final

--- src/main/java/org/adridadou/ethereum/propeller/values/EthAbi.java
+++ src/main/java/org/adridadou/ethereum/propeller/values/EthAbi.java
@@ -4,7 +4,7 @@ package org.adridadou.ethereum.propeller.values;
  * Created by davidroon on 25.04.17.
  * This code is released under Apache 2 license
  */
-public class EthAbi {
+public final class EthAbi {
     private final String abi;
 
     private EthAbi(String abi) {
