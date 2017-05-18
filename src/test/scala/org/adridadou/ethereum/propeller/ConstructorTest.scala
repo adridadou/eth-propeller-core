82e64a36f82c0f493243b72c1d2da022779e7af4
adridadou <david@roon.me>
2017-05-18 12:42:27 +0200

    add constructor tests

---
+++ src/test/scala/org/adridadou/ethereum/propeller/ConstructorTest.scala
@@ -0,0 +1,51 @@
+package org.adridadou.ethereum.propeller
+
+import java.io.File
+
+import org.adridadou.ethereum.propeller.backend.{EthereumTest, TestConfig}
+import org.adridadou.ethereum.propeller.exception.EthereumApiException
+import org.adridadou.ethereum.propeller.keystore.AccountProvider
+import org.adridadou.ethereum.propeller.values.EthValue.ether
+import org.adridadou.ethereum.propeller.values.SoliditySource
+import org.scalatest.check.Checkers
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by davidroon on 11.04.17.
+  * This code is released under Apache 2 license
+  */
+class ConstructorTest extends FlatSpec with Matchers with Checkers {
+
+  private val mainAccount = AccountProvider.fromSeed("hello")
+  private val ethereum = CoreEthereumFacadeProvider.create(new EthereumTest(TestConfig.builder.balance(mainAccount, ether(1000)).build), EthereumConfig.builder().build())
+  private val contractSource = SoliditySource.from(new File("src/test/resources/contractConstructor.sol"))
+
+  "Constructor" should "use the default constructor if no arguments are passed" in {
+    val compiledContract = ethereum.compile(contractSource).findContract("ContractConstructor").get
+    val address = ethereum.publishContract(compiledContract, mainAccount).get()
+    val myContract = ethereum.createContractProxy(compiledContract, address, mainAccount, classOf[ContractConstructor])
+    myContract.value() shouldEqual ""
+  }
+
+  "Constructor" should "use the parameter if given" in {
+    val compiledContract = ethereum.compile(contractSource).findContract("ContractConstructor").get
+    val address = ethereum.publishContract(compiledContract, mainAccount, "this is a test").get()
+    val myContract = ethereum.createContractProxy(compiledContract, address, mainAccount, classOf[ContractConstructor])
+    myContract.value() shouldEqual "this is a test"
+  }
+
+  "Constructor" should "show an error message if the constructor signature did not match the arguments" in {
+    val compiledContract = ethereum.compile(contractSource).findContract("ContractConstructor").get
+    try {
+      val address = ethereum.publishContract(compiledContract, mainAccount, 23938.asInstanceOf[java.lang.Integer]).get()
+      fail()
+    } catch {
+      case ex: EthereumApiException => ex.getMessage shouldEqual "No constructor found with params (Integer)"
+    }
+  }
+}
+
+trait ContractConstructor {
+  def value(): String
+}
+
