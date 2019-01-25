a21c41d56133faa2d48f16bd6d11759af5acbcd5
Felipe Forbeck <felipe.forbeck@gmail.com>
2019-01-25 15:09:03 -0200

    Changed the estimateGas function scope to public

--- src/test/scala/org/adridadou/ethereum/propeller/TransactionTest.scala
+++ src/test/scala/org/adridadou/ethereum/propeller/TransactionTest.scala
@@ -1,12 +1,20 @@
 package org.adridadou.ethereum.propeller
 
 
+import java.io.File
+
 import org.adridadou.ethereum.propeller.backend.{EthereumTest, TestConfig}
+import org.adridadou.ethereum.propeller.exception.EthereumApiException
 import org.adridadou.ethereum.propeller.keystore.AccountProvider
-import org.adridadou.ethereum.propeller.values.EthValue.ether
+import org.adridadou.ethereum.propeller.solidity.SolidityFunction
+import org.adridadou.ethereum.propeller.values.EthValue._
 import org.adridadou.ethereum.propeller.values._
+import org.apache.commons.lang.ArrayUtils
+import org.mockito.Mockito
 import org.scalatest.check.Checkers
 import org.scalatest.{FlatSpec, Matchers}
+
+import scala.compat.java8.OptionConverters._
 
 /**
   * Created by davidroon on 11.04.17.
@@ -29,7 +37,44 @@ class TransactionTest extends FlatSpec with Matchers with Checkers {
     result.error shouldBe empty
     result.isSuccessful shouldBe (true)
 
-    ethereum.getBalance(targetAccount) should be (oldBalance.plus(ether(1)))
+    ethereum.getBalance(targetAccount) should be(oldBalance.plus(ether(1)))
+  }
+
+  it should "estimate the gas usage of the transaction with ether and empty data" in {
+    val gasUsage = ethereum.estimateGas(ether(1), EthData.empty, mainAccount, targetAccount.getAddress)
+
+    gasUsage should not be (null)
+    wei(gasUsage.getUsage) should be(wei(221000))
+  }
+
+  it should "estimate the gas usage of the transaction with ether and some data" in {
+    val data = EthData.of("Test: Sending Transaction".getBytes())
+
+    val gasUsage = ethereum.estimateGas(ether(1), data, mainAccount, targetAccount.getAddress)
+
+    gasUsage should not be (null)
+    wei(gasUsage.getUsage) should be(wei(222700))
+  }
+
+  it should "estimate the gas usage of the contract creation transaction" in {
+    val ethereumBackend: EthereumBackend = Mockito.mock(classOf[EthereumBackend])
+    val proxy: EthereumProxy = Mockito.mock(classOf[EthereumProxy])
+    val contractSource = SoliditySource.from(new File("src/test/resources/contractConstructor.sol"))
+    val contract = ethereum.compile(contractSource).findContract("ContractConstructor").get
+    val constructorArgs = Array("This is a test").map(_.asInstanceOf[Object])
+    val argsEncoded: EthData = new SmartContract(contract, mainAccount, EthAddress.empty, proxy, ethereumBackend)
+      .getConstructor(constructorArgs).asScala
+      .map(constructor => constructor.encode("This is a test")).headOption match {
+      case Some(data) => data
+      case None => EthData.empty
+    }
+
+    val data = EthData.of(ArrayUtils.addAll(contract.getBinary.data, argsEncoded.data))
+
+    val gasUsage = ethereum.estimateGas(ether(0), data, mainAccount, EthAddress.empty)
+
+    gasUsage should not be (null)
+    wei(gasUsage.getUsage) should be(wei(394648))
   }
 
 }
