5c1a40e04e3f50f11a9a0cd127998528c19b3fa3
David Roon <david.roon@consensys.net>
2019-05-08 13:45:21 +0200

    update javadoc

--- src/main/java/org/adridadou/ethereum/propeller/EthereumFacade.java
+++ src/main/java/org/adridadou/ethereum/propeller/EthereumFacade.java
@@ -301,6 +301,7 @@ public class EthereumFacade {
      * Search an event definition from the ABI
      * @param contract The compiled contract
      * @param eventName The event name
+     * @param eventParams the type of each parameter in the event. Useful when you don't want to map it to a class
      * @return The solidity event definition if found
      */
     public Optional<RawSolidityEvent> findEventDefinitionForParameters(SolidityContractDetails contract, String eventName, List<Class<?>> eventParams) {
@@ -332,6 +333,7 @@ public class EthereumFacade {
      *
      * @param abi         The ABI
      * @param eventName   The event name
+     * @param eventParameters The types of each parameter in the event. Useful when you don't want to map the event to a class
      * @return The solidity event definition if found
      */
     public Optional<RawSolidityEvent> findEventDefinitionForParametersByAbi(EthAbi abi, String eventName, List<Class<?>> eventParameters) {
